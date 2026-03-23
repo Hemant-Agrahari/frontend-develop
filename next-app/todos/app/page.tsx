bc34603b61fc5beffce93e12f9c433ebd9645880
shawn <shawn@aistechnolabs.org>
2026-03-23 13:37:10 +0530

    Added todo

--- next-app/todos/app/page.tsx
+++ next-app/todos/app/page.tsx
@@ -4,18 +4,33 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [input, setInput] = useState("");
   const [items, setItems] = useState([]);
+  const [editId,setEditId]=useState()
   const add = () => {
     if (input === "") return null;
-    setItems((prev) => [...prev, input]);
+    if(editId !== null){
+      //update item
+      setItems((prev)=>prev.map((item)=>item.id === editId ? {...item,text:input} : item))
+      setEditId(null)
+    }else{
+      setItems((prev) => [...prev, {id:Date.now(),text:input}]);
+    }
+   
     setInput("");
   };
+
+  const deleteTodo = ()=>{
+
+  }
+  const editTodo = ()=>{
+
+  }
   return (
     <div className="d-flex align-items-center justify-content-center">
       <input type="text" onChange={(e) => setInput(e.target.value)} value={input}/>
-      <button onClick={add}>Add</button>
+      <button onClick={add}>{editId !== null ? "Update" : "Add"}Add</button>
       <ul>
         {items.map((items, index) => {
-          return <li key={index}>{items}</li>;
+          return <li key={index}>{items} <div><button onClick={deleteTodo}>delete</button> <button onClick={editTodo}>Edit</button></div></li>;
         })}
       </ul>
     </div>
